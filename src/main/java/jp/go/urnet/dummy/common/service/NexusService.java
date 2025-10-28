797ccacf265b4b57c1783af20fce5604011f85db
dev-nitta-ma@itg.hitachi.co.jp-at-038462777122 <nitta-ma@itg.hitachi.co.jp>
2025-10-28 18:27:37 +0900

    update

--- src/main/java/jp/go/urnet/dummy/common/service/NexusService.java
+++ src/main/java/jp/go/urnet/dummy/common/service/NexusService.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
- * ダミーサービスクラス
+ * Nexusサービスクラス
  * application.propertiesの設定値を提供する
  */
 @Component
